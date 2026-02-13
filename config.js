ad6e28228f70d7666d68a828820f7d1b1564d66b
sifeddinelounis16-wq <sifeddinelounis16@gmail.com>
2026-02-13 22:46:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -76,8 +76,8 @@ const CONFIG = {
     // Background Music (Optional)
     // Add your own music URL after getting proper licenses
     music: {
-        enabled: false,                     // Music feature is enabled
-        autoplay: false,                    // Try to autoplay (note: some browsers may block this)
+        enabled: true,                     // Music feature is enabled
+        autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/dzuogb2l3/video/upload/v1771017975/coldplay_-_A_Sky_Full_Of_Stars_Lyrics_mbdlw0.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
